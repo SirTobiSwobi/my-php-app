1cb39b1c0dce1cc68c4dd4aeb9e741c5e9aac1b4
SirTobiSwobi <SirTobiSwobi@users.noreply.github.com>
2016-06-27 11:13:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
 <?php 
-  echo "Hello World\n"; 
+  //echo "Hello World\n"; 
   
   $curl = curl_init('http://php-backend-demo-party.pcap.at/');
   $result = curl_exec($curl);
-  echo $result;
-
 
 ?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Hello World!</title>
+</head>
+
+<header>
+    <h1>Hello World!</h1>
+  </header>
+  <main>
+    <article>
+      <h1>
+        <?php
+           echo $result;
+        ?>
+      </h1>
+      <p>This is what the other service said.</p>
+    </article>
+  </main>
+  <footer>
+     <p>© 2016 by SirTobiSwobi</p>
+  </footer>
+
+</body>
+
+</html>
