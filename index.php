24ed96d7b73d78caa820b102f4559f84bffed167
SirTobiSwobi <SirTobiSwobi@users.noreply.github.com>
2016-06-27 10:53:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
   echo "Hello World\n"; 
   
-  $curl = curl_init('http://php-backend-demo-party.pcap.at/')
+  $curl = curl_init('http://php-backend-demo-party.pcap.at/');
   $result = curl_exec($curl);
   echo $result;
 
